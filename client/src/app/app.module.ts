a3d380deb34ff94222b47b4fb047d6d95fcd4a63
Isma1306 <isma1306@gmail.com>
2022-06-02 15:41:13 +0200

    Added file for custom service worker

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -32,7 +32,7 @@ import { environment } from '../environments/environment';
     NbCardModule,
     NbButtonModule,
     NbInputModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
+    ServiceWorkerModule.register('service-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
